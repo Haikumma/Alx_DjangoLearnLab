14d29406f5aab117f5b6b98fa2d9d76c3dd2157f
Mariam Hosni <mariam22hosni@gmail.com>
2024-12-14 23:54:54 +0100

    social media api

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -17,3 +17,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
