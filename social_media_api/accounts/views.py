650ee8ccc9101110a2d45c6686f6befe98a11428
Mariam Hosni <mariam22hosni@gmail.com>
2024-12-14 12:11:31 +0100

    commit

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import RegisterSerializer
+
 
 class RegisterView(APIView):
     def post(self, request):
