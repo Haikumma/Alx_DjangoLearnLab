96b6844f71dd62ba2c8bfd540891794a03245e35
Mariam Hosni <mariam22hosni@gmail.com>
2024-12-14 23:31:56 +0100

    social media api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import CommentSerializer, PostSerializer
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import viewsets
+from rest_framework import status
+from .models import Comment, Post
+
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthor]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title', 'content']
+    search_fields = ['title', 'content']
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthor]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title', 'content']
+    search_fields = ['title', 'content']
